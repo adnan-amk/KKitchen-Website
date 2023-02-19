148681e03100c78425872c43db2cdaf35faebcc8
adnan <amkadnan89@gmail.com>
2023-02-19 20:31:13 +0530

    Home with swipper and images

--- js/script.js
+++ js/script.js
@@ -18,3 +18,22 @@ document.querySelector('#search-icon').onclick=()=>{
 document.querySelector('#close').onclick=()=>{
     document.querySelector('#search-form').classList.remove('active');
 }
+
+// Swipper Script
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 500,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+   loop:true,
+   navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  });
